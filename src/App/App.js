6761f73b2b7ec5fb7ba67bad501517f7c0c576a3
Sem Thijssen <30442@ma-web.nl>
2022-10-09 14:16:40 +0200

    videos 6 t/m 8

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,10 @@ import CounterCard from "../CounterCard/CounterCard";
 import "./App.css";
 const App = ()=>{
     return(
-        <CounterCard/>
+        <>
+        <CounterCard number="10"/>
+        <CounterCard number="100"/>
+        </>
     );
 }
 export default App;
--- src/CounterCard/CounterCard.js
+++ src/CounterCard/CounterCard.js
@@ -1,18 +1,29 @@
+import React from "react";
+import "./CounterCard.css";
+class CounterCard extends React.Component{
 
-import "./CounterCard.css"
-const CounterCard = () => {
-    return(
+   increase = () => {
+        let test = document.getElementById("js--number");
+      test.innerText = parseInt(test.innerText) + 1;
+    }
+    
+   decrease = () =>{
+        let test = document.getElementById("js--number");
+        test.innerText = parseInt(test.innerText) - 1;
+    }
+    render(){ return(
         <article class ="counter">
 <section class="counter__number">
-<h1 class="counter__value">0</h1>
+<h1 class="counter__value" id="js--number">{this.props.number}</h1>
 </section>
 <section class="counter__buttons">
     
-<button className="counter__button counter__button--Decrease">Decrease</button>
-<button className="counter__button counter__button--Increase">Increase</button>
+<button onClick={this.decrease}className="counter__button counter__button--Decrease">Decrease</button>
+<button onClick={this.increase}className="counter__button counter__button--Increase">Increase</button>
 </section>
         </article>
-    );
+    );}
+   
        
     
 }
